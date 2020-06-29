02dcd5fcfa125a13ae07535ef39478b0933f6ef8
iuri-pdista <coreaiuri86@gmail.com>
2020-06-29 16:53:51 -0300

    Added the click function

--- bot.py
+++ bot.py
@@ -1,7 +1,15 @@
 from selenium import webdriver
     
 
-    class NikeBot():
-        def _init_(self):
-            self.driver = webdriver.chrome()
-            
+   class NikeBot():
+     def _init_(self):
+       self.driver = webdriver.chrome()
+        
+     def click(self , XPath): 
+        elem = self.driver.get_element_by_xpath(XPath)
+        if (elem):
+            elem.click()
+            return elem
+        else 
+            print ("Element not found")
+            return False              
