5edb6c9b3659f158c4fced8d6fd25d8277735fc3
Guilherme Guatura <guaturadzn@gmail.com>
2020-07-02 01:21:33 -0300

    add get_size func

--- bot.py
+++ bot.py
@@ -1,10 +1,10 @@
-from time import sleep
-
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 class NikeBot:
-
     URL = "https://www.nike.com.br/Snkrs#estoque"
 
     PATH_EDGE = "./edgedriver_win64/msedgedriver.exe"
@@ -25,8 +25,9 @@ class NikeBot:
     def open_url(self):
         try:
             self.driver.get(self.URL)
-        except:
+        except Exception as e:
             print("Please use a valid browser")
+            print(e)
             exit(1)
 
     def click(self, xpath):
@@ -42,54 +43,26 @@ class NikeBot:
         xpath = "//*[@id=\"DadosPaginacaoEstoque\"]/div[1]/div[1]/div/div[2]/a"
         link = self.driver.find_element_by_xpath(xpath).get_attribute("href")
         self.driver.get(link)
-    
-    def get_size(self, Size_option1, Size_option2):
-        try:
-            Size_option1 = self.driver.find_element_by_id("tamanho__id" + Size_option1)
-            Size_option1.get_attribute("disabled") != "disabled" 
-            Size_option1.click()
-        except:
-            Size_option2 = self.driver.find_element_by_id("tamanho__id" + Size_option2)
-            Size_option2.get_attribute("disabled") != "disabled"
-            Size_option1.click()
-        else:
-            print ("The values you selected are not available")
-            #we need to think what to do when the sizes are not available, because exitting and running the script all again is not a possibility
 
-    def login_checker (self):
-        #there is two ways of doing it
-        # FIRST
-        #xpath = "//*[@id=\"header\"]/div[1]/div/div/div[2]/span[1]/span[3]"
-        #ClassPicker = self.driver.find_element_by_xpath(xpath).get_attribute("class")
-        #if "nao_logado" in ClassPicker:
-        #    print ("You are not logged in")
-        #    xpath_to_login = "//*[@id=\"anchor-acessar\"]"
-        #    xpath_to_login.click()
-        #    print ("Please Login")
-        #else:
-        #    return True  
-        #
-        # SECOND
-        xpath = "//*[@id=\"header\"]/div[1]/div/div/div[2]/span[1]/span[1]"
+    def get_size(self, size_option1, size_option2):
+
         try:
-            self.driver.find_element_by_xpath(xpath)
-            print ("You are logged in")
-            return True
+            elem = WebDriverWait(self.driver, 5).until(
+                EC.presence_of_element_located((By.ID, f'tamanho__id{size_option1}'))
+            )
+            self.driver.execute_script("arguments[0].checked = true;", elem)
+            print(f'selected shoe size: {size_option1}')
+
         except:
-            xpath_to_login = "//*[@id=\"anchor-acessar\"]"
-            xpath_to_login.click()
-            print ("Please Login")
-    def add_to_cart (self):
-        xpath = "//*[@id=\"btn-comprar\"]"              
-        click(xpath)
-    def finalize(self):
-        xpath_cart = "//*[@id=\"carrinho\"]/div[3]/div[5]/a"
-        xpath_identification = "//*[@id=\"seguir-pagamento\"]"
-        xpath_payment = "//*[@id=\"confirmar-pagamento\"]"
-        click(xpath_cart)
-        #we need to insert a little pause to the page load
-        click(xpath_identification)
-        #same
-        #we need to create exceptions to check if the fields are filled
-        click(xpath_payment)
-        
+
+            try:
+                elem = WebDriverWait(self.driver, 1).until(
+                    EC.presence_of_element_located((By.ID, f'tamanho__id{size_option2}'))
+                )
+                self.driver.execute_script("arguments[0].checked = true;", elem)
+                print(f'selected shoe size: {size_option2}')
+
+            except Exception as error:
+                print("invalid shoe size")
+                print(error)
+                self.driver.quit()
