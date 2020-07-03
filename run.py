f49246c2d757df13be52eac1033c820954a47f09
Guilherme Guatura <guaturadzn@gmail.com>
2020-07-03 04:35:31 -0300

    improve features functionality

--- run.py
+++ run.py
@@ -1,7 +1,39 @@
-from time import sleep
-
-from bot import NikeBot
 import sys
+from bot import NikeBot
+
+
+def show_logo():
+    logo = """
+             .........                   .........                     
+           @@@@@@@@@@@@                @@@@@@@@@@@@                   
+           @@@@@@@@@@@@                @@@@@@@@@@@@                   
+           @@@@@@@@@@@@                @@@@@@@@@@@@                   
+           @@@@@@@@@@@@                @@@@@@@@@@@@                   
+                                                                      
+                                                                      
+                                                      %          
+           (@                                    @@@@&                
+          @@                          ,@@@@@@@@&                      
+        @@@@               .@@@@@@@@@@@@@&                            
+       @@@@@@@@@@@@@@@@@@@@@@@@@@@@@                                  
+       @@@@@@@@@@@@@@@@@@@@@@&                                        
+       @@@@@@@@@@@@@@@@@                                              
+        @@@@@@@@@%  
+        
+        
+       ███▄    █  ██▓ ██ ▄█▀▓█████     ▄▄▄▄    ▒█████  ▄▄▄█████▓
+       ██ ▀█   █ ▓██▒ ██▄█▒ ▓█   ▀    ▓█████▄ ▒██▒  ██▒▓  ██▒ ▓▒
+      ▓██  ▀█ ██▒▒██▒▓███▄░ ▒███      ▒██▒ ▄██▒██░  ██▒▒ ▓██░ ▒░
+      ▓██▒  ▐▌██▒░██░▓██ █▄ ▒▓█  ▄    ▒██░█▀  ▒██   ██░░ ▓██▓ ░ 
+      ▒██░   ▓██░░██░▒██▒ █▄░▒████▒   ░▓█  ▀█▓░ ████▓▒░  ▒██▒ ░ 
+      ░ ▒░   ▒ ▒ ░▓  ▒ ▒▒ ▓▒░░ ▒░ ░   ░▒▓███▀▒░ ▒░▒░▒░   ▒ ░░   
+      ░ ░░   ░ ▒░ ▒ ░░ ░▒ ▒░ ░ ░  ░   ▒░▒   ░   ░ ▒ ▒░     ░    
+         ░   ░ ░  ▒ ░░ ░░ ░    ░       ░    ░ ░ ░ ░ ▒    ░      
+               ░  ░  ░  ░      ░  ░    ░          ░ ░           
+                                            ░          
+    [*] Script made by: guiguat and iuri-pdista
+    """
+    print(logo)
 
 
 def send_help(e):
@@ -35,6 +67,7 @@ def initialize_script():
 
                 elif sys.argv[i] == "-s" or sys.argv[i] == "--size":
                     size_param = sys.argv[i + 1] if sys.argv[i + 1] else ""
+
                 elif sys.argv[i] == "-n" or sys.argv[i] == "--name":
                     name_param = sys.argv[i + 1] if sys.argv[i + 1] else ""
 
@@ -43,12 +76,12 @@ def initialize_script():
 
             i += 1
 
-        if browser_name == "":
+        if browser_name == "" or email_param == "" or pwd_param == "" or size_param == "":
             send_help("")
         else:
             return browser_name, email_param, pwd_param, size_param, name_param
     else:
-        print("You didn't pass any arguments")
+        print("[*] You didn't pass any arguments")
         send_help("")
 
 
@@ -58,12 +91,17 @@ size = str(size).strip().split(",")
 if email == "" or password == "":
     send_help("")
 
+show_logo()
 bot = NikeBot(browser)
 bot.driver.get("https://www.nike.com.br/Snkrs")
 bot.click_login()
 bot.login(email, password)
 bot.open_url()
-# bot.get_product()
-bot.alt_get_product(name)
-#  bot.get_size(size[0], size[1])
-#  bot.click_buy()
+
+if name != "":
+    bot.alt_get_product(name)
+else:
+    bot.get_product()
+
+bot.get_size(size[0], size[1])
+bot.click_buy()
